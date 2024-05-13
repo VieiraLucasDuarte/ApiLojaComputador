328cbb54d7d2e11467719ac8d869fc906aae3011
Lucas <lucasduartevieira0@gmail.com>
2024-05-12 21:52:06 -0300

    ✨:Adicionado um anova configuração de CORS

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,13 @@
-INSERT INTO PRODUTO (CATEGORIA_ID, NOME, Descricao_Curta, Descricao_Longa, valor, promocao, quantidade) values (NULL, 'com', 'Computador bem do top', 'Computador gigantesco', 10.0, true, 100)
+INSERT INTO CATEGORIA (DESCRICAO, ATIVO) values ('CATEGORIA PC', true);
+
+INSERT INTO CATEGORIA (DESCRICAO, ATIVO) values ('HARDWARE', true);
+
+INSERT INTO CATEGORIA (DESCRICAO, ATIVO) values ('PERIFERICO', true);
+
+
+INSERT INTO PRODUTO (CATEGORIA_ID, NOME, Descricao_Curta, Descricao_Longa, valor, promocao, quantidade) values (1, 'Computador', 'Computador bem do top', 'Computador gigantesco', 10.0, true, 100);
+
+INSERT INTO PRODUTO (CATEGORIA_ID, NOME, Descricao_Curta, Descricao_Longa, valor, promocao, quantidade) values (1, 'TECLADO', 'teclado do luz', 'teclado com luz bem grande', 10.0, true, 100);
+
+
+
