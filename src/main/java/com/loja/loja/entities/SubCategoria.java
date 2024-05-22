1385a5f6eb9a1311632b6a22d5e4efb298677ce4
Lucas <lucasduartevieira0@gmail.com>
2024-05-21 21:20:20 -0300

    ✨:Adicionado nova entidade de subcategoria

---
+++ src/main/java/com/loja/loja/entities/SubCategoria.java
@@ -0,0 +1,40 @@
+package com.loja.loja.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table()
+public class SubCategoria {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private int id;
+   private String descricao;
+   private boolean ativo;
+
+   public SubCategoria() {
+   }
+
+   public int getId() {
+      return id;
+   }
+
+   public void setId(int id) {
+      this.id = id;
+   }
+
+   public String getDescricao() {
+      return descricao;
+   }
+
+   public void setDescricao(String descricao) {
+      this.descricao = descricao;
+   }
+
+   public boolean isAtivo() {
+      return ativo;
+   }
+
+   public void setAtivo(boolean ativo) {
+      this.ativo = ativo;
+   }
+}
